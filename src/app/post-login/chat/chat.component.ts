367c420ad5a73f55aaecaf99bfc642a9bcc61331
Dragan <illutas1987@gmail.com>
2024-06-27 10:17:13 +0200

    refactor: Update chat component to display user avatars dynamically
    The code changes in the chat component HTML and SCSS files update the avatar display to dynamically show user avatars based on the number of users. The previous static avatar images have been replaced with a loop that iterates over the users array and displays the profile images. This improves the flexibility and scalability of the chat component.

--- src/app/post-login/chat/chat.component.ts
+++ src/app/post-login/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -9,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class ChatComponent {
 
+  users = [
+    {
+      id: 1,
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'Ä',
+      phone: '1234567890',
+      profileImg: 'assets/img/character-images/character_1.png'
+    },
+    {
+      id: 2,
+      firstname: 'Jane',
+      lastname: 'Smith',
+      email: 'B',
+      phone: '9876543210',
+      profileImg: 'assets/img/character-images/character_2.png'
+    },
+    {
+      id: 3,
+      firstname: 'Alice',
+      lastname: 'Johnson',
+      email: 'C',
+      phone: '5555555555',
+      profileImg: 'assets/img/character-images/character_3.png'
+    },
+  ]
 }
