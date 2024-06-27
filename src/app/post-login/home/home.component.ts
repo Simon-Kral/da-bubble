aef52f3160bccf922965a0a43ec0d3330fccf738
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-06-27 13:55:33 +0200

    Sidenav controller implemented.
    User-friendliness is improved by the ability to show and hide the Sidenav so that other components can be displayed larger.
    
    Animations will be improved later.

--- src/app/post-login/home/home.component.ts
+++ src/app/post-login/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { ChatComponent } from '../chat/chat.component';
 import { SidenavComponent } from "../sidenav/sidenav.component";
@@ -10,8 +11,16 @@ import { ThreadComponent } from '../thread/thread.component';
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, ChatComponent, SidenavComponent, ThreadComponent]
+    imports: [CommonModule,HeaderComponent, ChatComponent, SidenavComponent, ThreadComponent]
 })
 export class HomeComponent {
+ //sidenav
+ isSidenavVisible: boolean = true;
 
+ toggleSidenav() {
+    console.log("toggleSidebar");
+    this.isSidenavVisible = !this.isSidenavVisible;
+ }
+ 
+ 
 }
