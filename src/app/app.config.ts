f6e8a311e1faf00cbfe52e7e1732aa7a3253a0ae
Simon Kral <simonkral92@gmail.com>
2024-06-27 08:38:35 +0200

    login-screen structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,8 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"da-bubble-b7d76","appId":"1:829050807456:web:c04f2d844cab035f4f55c5","storageBucket":"da-bubble-b7d76.appspot.com","apiKey":"AIzaSyDlnzbLdBSdYINIBM1v4OcfHW2Pr-LPBH4","authDomain":"da-bubble-b7d76.firebaseapp.com","messagingSenderId":"829050807456"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"da-bubble-b7d76","appId":"1:829050807456:web:c04f2d844cab035f4f55c5","storageBucket":"da-bubble-b7d76.appspot.com","apiKey":"AIzaSyDlnzbLdBSdYINIBM1v4OcfHW2Pr-LPBH4","authDomain":"da-bubble-b7d76.firebaseapp.com","messagingSenderId":"829050807456"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideAnimations()]
 };
