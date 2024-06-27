f46cfca3ef66171cd42d9de254f102b555d415a3
Dragan <illutas1987@gmail.com>
2024-06-27 10:28:13 +0200

    The code changes in the home component HTML and TypeScript files add the thread component to the home page.

--- src/app/post-login/home/home.component.ts
+++ src/app/post-login/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ChatComponent } from '../chat/chat.component';
+import { ThreadComponent } from '../thread/thread.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ChatComponent],
+  imports: [HeaderComponent, ChatComponent, ThreadComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
