474acc9254144125e784274430c2da328c03ddfa
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-06-27 10:37:01 +0200

    created sidenav component and started to implement HTML / CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './post-login/home/home.component';
+import { SidenavComponent } from './post-login/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/post-login/home/home.component.ts
+++ src/app/post-login/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ChatComponent } from '../chat/chat.component';
+import { SidenavComponent } from "../sidenav/sidenav.component";
 import { ThreadComponent } from '../thread/thread.component';
 
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [HeaderComponent, ChatComponent, ThreadComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [HeaderComponent, ChatComponent, SidenavComponent, ThreadComponent]
 })
 export class HomeComponent {
 
