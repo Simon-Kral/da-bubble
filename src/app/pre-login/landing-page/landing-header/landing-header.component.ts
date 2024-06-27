b067af51905da885150603bb06767a48c1a06714
Simon Kral <simonkral92@gmail.com>
2024-06-27 14:57:14 +0200

    added intro-animation

--- src/app/pre-login/landing-page/landing-header/landing-header.component.ts
+++ src/app/pre-login/landing-page/landing-header/landing-header.component.ts
@@ -1,37 +1,82 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, state, style, transition, trigger } from '@angular/animations';
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+
+const enterIntro = transition(":enter", [
+  group([
+    query(".logo_with_text > img", [
+      style({
+        marginLeft: 'calc(589px / 2 - 70px)',
+      }),
+      animate('200ms 500ms', style({
+        marginLeft: '0',
+      }))
+    ]),
+    query(".span_wrap > span", [
+      style({
+        marginLeft: '-500px',
+      }),
+      animate('300ms 900ms', style({
+        marginLeft: '0%',
+      }))
+    ]),
+    query(".logo_with_text", [
+      style({
+        height: '140px',
+        top: 'calc(50vh - 70px)',
+        left: 'calc(50% - 589px / 2)',
+      }),
+      animate('500ms 2.5s',
+        style({
+          height: '70px',
+          top: '0',
+          left: '0',
+        })
+      )
+    ]),
+    query(".span_wrap > span", [
+      style({
+        fontSize: '90px',
+        color: 'white'
+      }),
+      animate('500ms 2.5s', style({
+        fontSize: '24px',
+        color: 'black'
+      }))
+    ]),
+    query(".overlay", [
+      style({
+        opacity: '1'
+      }),
+      animate('500ms 2.5s', style({
+        opacity: '0'
+      }))
+    ])
+  ])
+])
+
+const intro = trigger("intro", [enterIntro]);
 
 @Component({
   selector: 'app-landing-header',
   standalone: true,
-  imports: [],
-  animations: [
-    trigger('move', [
-      state('false', style({
-        height: '140px'
-      })),
-      state('true', style({
-        height: '70px',
-        top: '0',
-        left: '0',
-      })),
-      transition('* => *', animate('500ms 2.5s')) // orig
-      // transition('* => *', animate('500ms ease')) // testing
-      // ,
-      // state('true', style({
-
-      // })
-      // )
-    ])
+  imports: [
+    NgIf
   ],
+  animations: [intro],
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.scss'
 })
 export class LandingHeaderComponent {
-  moveStarted: boolean = false;
-  expandStarted: boolean = false;
+  introAnimationStarted: boolean = false;
+  introAnimationFinished: boolean = false;
   
   constructor() {
-    this.moveStarted = true;
+    this.introAnimationStarted = true;
+  }
+
+  onAnimationDone(event: any) {
+    console.log("onAnimationDone", event);
+    this.introAnimationFinished = true;
   }
 }
