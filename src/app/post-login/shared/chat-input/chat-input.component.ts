c023b11387aaf90452e2ae9c27dda31f53ee7a42
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-07-01 00:02:41 +0200

    refactor: Update chat input component to use dynamic image sources

--- src/app/post-login/shared/chat-input/chat-input.component.ts
+++ src/app/post-login/shared/chat-input/chat-input.component.ts
@@ -1,13 +1,18 @@
-
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-chat-input',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './chat-input.component.html',
-  styleUrl: './chat-input.component.scss'
+  styleUrl: './chat-input.component.scss',
 })
 export class ChatInputComponent {
   @Output() messageEvent = new EventEmitter<object>();
@@ -19,9 +24,11 @@ export class ChatInputComponent {
 
   constructor(private fb: FormBuilder) {
     this.messageData = this.fb.group({
-      message: new FormControl('', [Validators.required, Validators.minLength(1)])
+      message: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
     });
-
   }
 
   /**
@@ -38,13 +45,13 @@ export class ChatInputComponent {
    * Emits the message to be sent and resets the form.
    */
   onSubmit(): void {
-    if (this.messageData.invalid){
+    if (this.messageData.invalid) {
       return;
     }
 
     const messageToSend = {
       timestamp: this.getCurrentTime(),
-      message: this.messageData.value.message
+      message: this.messageData.value.message,
     };
 
     this.messageEvent.emit(messageToSend);
@@ -52,23 +59,24 @@ export class ChatInputComponent {
   }
 
   onMouseOver(imgName: string): void {
-    if(imgName === 'add') {
+    if (imgName === 'add') {
       this.iconSourceAdd = 'assets/img/icons/add_blue.png';
-    } else if(imgName === 'smiley') {
+    } else if (imgName === 'smiley') {
       this.iconSourceSmiley = 'assets/img/icons/smiley_blue.png';
-    } else if(imgName === 'alternate_email') {
-      this.iconSourceAlternateEmail = 'assets/img/icons/alternate_email_blue.png';
+    } else if (imgName === 'alternate_email') {
+      this.iconSourceAlternateEmail =
+        'assets/img/icons/alternate_email_blue.png';
     }
   }
 
   onMouseOut(imgName: string): void {
-    if(imgName === "add") {
+    if (imgName === 'add') {
       this.iconSourceAdd = 'assets/img/icons/add_grey.png';
-    } else if(imgName === 'smiley') {
+    } else if (imgName === 'smiley') {
       this.iconSourceSmiley = 'assets/img/icons/smiley_grey.png';
-    }
-    else if(imgName === 'alternate_email') {
-      this.iconSourceAlternateEmail = 'assets/img/icons/alternate_email_grey.png';
+    } else if (imgName === 'alternate_email') {
+      this.iconSourceAlternateEmail =
+        'assets/img/icons/alternate_email_grey.png';
     }
   }
 }
