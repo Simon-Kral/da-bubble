9f5546dc8ba4966d56729ae6b48b3793be2589cc
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-05 17:37:26 +0200

    Updated: Adjusted some CSS in Home/Header/new & private message component

--- src/app/post-login/new-message/new-message.component.ts
+++ src/app/post-login/new-message/new-message.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatInputComponent } from '../shared/chat-input/chat-input.component';
 
 @Component({
   selector: 'app-new-message',
   standalone: true,
-  imports: [],
+  imports: [ChatInputComponent],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.scss'
 })
