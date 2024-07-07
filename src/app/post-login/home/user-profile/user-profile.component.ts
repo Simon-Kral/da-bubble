07b9d9cbae59fc559126d3d2a2fa6ab17e7623ac
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-07 23:08:04 +0200

    Update: adjustes some scss & implemented funktion to close user-profile

--- src/app/post-login/home/user-profile/user-profile.component.ts
+++ src/app/post-login/home/user-profile/user-profile.component.ts
@@ -10,13 +10,37 @@ import { CommonModule } from '@angular/common';
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
-
   firebaseService = inject(FirebaseService);
 
- 
   @Input() isUserProfileVisible: boolean = false;
   @Output() userProfileVisibilityChange = new EventEmitter<boolean>();
 
   showDataOfUser = this.firebaseService.selectedPrivateChatCreatorId; 
+  // Default icon sources
+  close = 'assets/img/icons/close_black.png';
+  // Hover icon sources
+  closeHover = 'assets/img/icons/close_blue.png';
+  // current Icon Source
+  currentIconSourceClose = this.close;
+
+  closeUserProfile(): void {
+    this.userProfileVisibilityChange.emit(false);
+  }
+
+  	/**
+	 * Handles the mouse over event for the sideNav icons.
+	 * Changes the icon source based on the provided image name.
+	 */
+	onMouseOver(): void {
+    this.currentIconSourceClose = this.closeHover;
+	}
+
+	/**
+	 * Handles the mouse out event for the specified image.
+   * Changes the icon source based on the provided image name.
+	 */
+	onMouseOut(): void {
+    this.currentIconSourceClose = this.close;
+	}
 
 }
