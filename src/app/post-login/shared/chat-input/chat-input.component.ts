01e5682883e1760032d01b44514c8a76347939ff
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-08 01:44:53 +0200

    Feat: ngx-emoji-mart installed & implemented in shared chat-input component

--- src/app/post-login/shared/chat-input/chat-input.component.ts
+++ src/app/post-login/shared/chat-input/chat-input.component.ts
@@ -6,11 +6,12 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-chat-input',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PickerComponent],
   templateUrl: './chat-input.component.html',
   styleUrl: './chat-input.component.scss',
 })
@@ -27,6 +28,10 @@ export class ChatInputComponent {
   iconSourceSmiley = 'assets/img/icons/smiley_grey.png';
   iconSourceAlternateEmail = 'assets/img/icons/alternate_email_grey.png';
   iconSourceSend = 'assets/img/icons/send_dark_blue.png';
+
+  //emoji picker
+  showEmojiPicker = false;
+
 
   constructor(private fb: FormBuilder) {
     this.messageData = this.fb.group({
@@ -67,6 +72,24 @@ export class ChatInputComponent {
     this.messageData.reset();
   }
 
+  //emoji code
+  /**
+   * Toggles the emoji picker visibility.
+   */
+  toggleEmojiPicker() {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+  /**
+  * Adds the selected emoji to the message text and updates the form control.
+  * @param event The event object containing the selected emoji.
+  *              The emoji is expected to have a 'native' property representing its textual representation.
+   */
+  addEmoji(event: { emoji: { native: any; }; }) {
+    let { message } = this.messageData.value;
+    let text = `${message}${event.emoji.native}`;
+
+    this.messageData.setValue({ message: text });
+  }
   /**
    * Handles the mouse over event for the chat input icons.
    * Changes the icon source based on the provided image name.
