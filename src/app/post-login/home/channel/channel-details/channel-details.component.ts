02738baf870036ab96b17e87cbed41045e3fbc5c
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-07-09 20:11:06 +0200

    refactor: Update channel details component structure and design

--- src/app/post-login/home/channel/channel-details/channel-details.component.ts
+++ src/app/post-login/home/channel/channel-details/channel-details.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { ChatService } from '../../../../services/chat/chat.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel-details.component.html',
   styleUrl: './channel-details.component.scss'
 })
@@ -16,4 +17,15 @@ export class ChannelDetailsComponent {
 	
 	chatService = inject(ChatService);
 
+	isChannelNameEditable = false;
+
+	toggleIsChannelNameEditable() {
+		this.isChannelNameEditable = !this.isChannelNameEditable;
+		console.log('isChannelNameEditable', this.isChannelNameEditable);
+	}
+
+	saveChannelName() {
+		this.isChannelNameEditable = !this.isChannelNameEditable;
+		console.log('saved channel name');
+	}
 }
