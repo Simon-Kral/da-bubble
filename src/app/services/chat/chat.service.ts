733e8060c00917f06ea8dfaa060e056f92c0e039
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-10 14:57:16 +0200

    Update: jsDoc & to-do comments added in chat service & prvt-msg.list component

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -32,7 +32,7 @@ export class ChatService implements OnInit{
   ngOnDestroy() {
     if (this.unsubscribeMsgList) {
       this.unsubscribeMsgList();
-    }
+    }    // to-do: implement unsubscribe to private message list
   }
 
   onMessageSent(event: { message: string, source: string, destinationCollection:string, destinationDocRef:string, timestamp: number }) {
@@ -50,7 +50,11 @@ export class ChatService implements OnInit{
 toggleChannelDetailsVisibility(visible: boolean) {
 	  this.isChannelDetailsVisibleSource.next(visible);
 }
-
+/**
+ * Subscribes to the messages subcollection and updates the message list in real-time.
+ * Orders the messages by the 'time' field.
+ * to-do: implemet better orderBy + variabel to subscribe wether to channels or private messages collection
+ */
 subscribeMsgList() {
   const q = query(this.getMsgSubColRef(), orderBy('time'));
   this.unsubscribeMsgList = onSnapshot(
@@ -68,11 +72,22 @@ subscribeMsgList() {
     }
   );
 }
+/**
+ * Gets a reference to the messages subcollection for the currently selected privateChats collection or the channels collection.
+ * 
+ * to-do: implement variabel  for channels or private messages collection
+ */
 
 getMsgSubColRef() {
   return collection(this.firestore,`privateChats/${this.firebaseService.selectedPrivateChatId}/messages`);
 }
-
+/**
+ * Formats the message data into a Message object.
+ * 
+ * @param {any} obj - The message data from the Firestore document.
+ * @param {string} id - The ID of the Firestore document.
+ * @returns {Message} The formatted message object.
+ */
 setMessage(obj: any, id: string): Message{
   return {
     messageId: id || '',
