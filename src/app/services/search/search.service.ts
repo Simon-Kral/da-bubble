c908e9c26b43e5d6d5d5abbea88935a660f914d4
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-11 23:23:46 +0200

    refactor: serach service
    removed unnecessary code which was needed for testing

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,28 +1,30 @@
 import { inject, Injectable } from '@angular/core';
 import { FirebaseService } from '../firebase/firebase.service';
 import { Observable, Subject } from 'rxjs';
+import { collection, CollectionReference, DocumentData, getDocs, query, where } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  
+  firestore = inject(FirebaseService).firestore;
   firebaseService = inject(FirebaseService);
   constructor() { }
 
   userSearchResults = [];
   channelSearchResults = [];
-  searchText = new Subject<{ text: string, source: string }>();
+  messageSearchResults = [];
 
-  setSearchText(searchObj: { text: string, source: string }) {
-    this.searchText.next(searchObj);
+
+  searchText = '';
+
+
+
+  setSearchText(searchValue: string) {
+    this.searchText = searchValue;
+    console.log('Search executed with value:', searchValue);
   }
 
-  onSearch(searchValue: string, source: string) {
-    let searchObj = { text: searchValue, source };
-    this.setSearchText(searchObj);
-    console.log('Search executed with value:', searchValue, 'from:', source);
-  }
 
 
 }
