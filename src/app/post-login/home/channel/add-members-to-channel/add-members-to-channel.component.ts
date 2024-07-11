698c3a73877e781f4ebd58f4c0ec24392724d201
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-07-11 10:54:29 +0200

    refactor: Update channel details component to use dynamic channel name in HTML template

--- src/app/post-login/home/channel/add-members-to-channel/add-members-to-channel.component.ts
+++ src/app/post-login/home/channel/add-members-to-channel/add-members-to-channel.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input, Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
+
+import { FormsModule, NgModel } from '@angular/forms';
+import { FirebaseService } from './../../../../services/firebase/firebase.service';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+
 
 @Component({
-  selector: 'app-add-members-to-channel',
-  standalone: true,
-  imports: [],
-  templateUrl: './add-members-to-channel.component.html',
-  styleUrl: './add-members-to-channel.component.scss'
+	selector: 'app-add-members-to-channel',
+	standalone: true,
+	imports: [FormsModule, NgClass],
+	templateUrl: './add-members-to-channel.component.html',
+	styleUrl: './add-members-to-channel.component.scss',
 })
 export class AddMembersToChannelComponent {
 	@Input() isAddMembersToChannelVisible = false;
 	@Output() addMembersToChannelVisibilityChange = new EventEmitter<boolean>();
-	
+	userName = '';
+	firebaseService = inject(FirebaseService);
+
 }
