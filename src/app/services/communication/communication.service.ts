9673f38d62ad073c4321d1b67c9bc4eb7f6a8e7e
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-12 23:13:48 +0200

    refactor: thread variable outsourced from home to communication service

--- src/app/services/communication/communication.service.ts
+++ src/app/services/communication/communication.service.ts
@@ -8,7 +8,7 @@ export class CommunicationService {
 	//prvt msg component
 	showPrivateNote: boolean = false;
 
-	// moved from chat.service -- varaibels for currentUser & userProfile visibility
+	// moved from chat.service -- variabels for currentUser & userProfile visibility
 
 	isCurrentUserProfileVisibleSource = new BehaviorSubject<boolean>(false);
 	isCurrentUserProfileVisible$ = this.isCurrentUserProfileVisibleSource.asObservable();
@@ -17,19 +17,30 @@ export class CommunicationService {
 	
 	isChannelDetailsVisibleSource = new BehaviorSubject<boolean>(false);
 	isChannelDetailsVisible$ =this.isChannelDetailsVisibleSource.asObservable();
+
 	isAddMembersToChannelVisibleSource = new BehaviorSubject<boolean>(false);
 	isAddMembersToChannelVisible$ = this.isAddMembersToChannelVisibleSource.asObservable();
+
+	// moved from home.component -- variabels thread visibility
+	//thread variables
+	isThreadVisible: boolean = false;
+
 
 	constructor() {}
 
 	//prvt msg component
-	togglePrivateNote(trueOrFalse: boolean) {
-		if (trueOrFalse) {
+	togglePrivateNote(visible: boolean) {
+		if (visible) {
 			this.showPrivateNote = true;
 		} else {
 			this.showPrivateNote = false;
 		}
 	}
+
+	toggleThreadVisibility(visible: boolean) {
+		this.isThreadVisible = !this.isThreadVisible;
+	}
+
 
 	toggleCurrentUserProfileVisibility(visible: boolean) {
 		this.isCurrentUserProfileVisibleSource.next(visible);
