0b52d8b50698b6f60ff3c6baa03e419aa7ec40b1
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-16 20:27:46 +0200

    feat: build on searchService started & first functions added
    This service receives search requests from all components and processes them. First, a searchUsersByName function was written as an observable. This processes the received search text and searches the Users collection and pushes the results into an array after comparing the search text. The contents of the array are rendered in the channel-member-selection, among other things. A pushSelectedUsersToArray function was also written. If you click on a user in the channel-member-selection, they are pushed into the selectedUser array. There is a query so that a member cannot be added twice. The last function written was removeSelectedUserFormArray. If you want to delete a user that has already been added from the selectedMembers array, then you can use this function.

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,24 +1,66 @@
 import { inject, Injectable } from '@angular/core';
 import { FirebaseService } from '../firebase/firebase.service';
-import { Observable, Subject } from 'rxjs';
-import { collection, CollectionReference, DocumentData, getDocs, query, where } from '@angular/fire/firestore';
+import { map, Observable, Subject } from 'rxjs';
+import { query,orderBy,where,Firestore,collection,doc,onSnapshot,updateDoc,getDocs, arrayUnion, collectionData } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  firestore = inject(FirebaseService).firestore;
+  firestore: Firestore = inject(Firestore);
   firebaseService = inject(FirebaseService);
   constructor() { }
 
-  userSearchResults = [];
+  userSearchResults: string[] = [];
   channelSearchResults = [];
   messageSearchResults = [];
 
+  selectedUser: string [] = [];
+  selectedChannel = [];
+  selectedMessage = [];
+
 
   searchText = '';
+  memberSearchActive: boolean = false;
 
 
 
 
+  /**
+   * Searches the users collection for documents where the name field matches or contains the search text.
+   * @param searchText The text to search for in user names.
+   * @returns An Observable of the search results.
+   */
+  searchUsersByName(): Observable<any[]> {
+    const usersCollection = collection(this.firestore, 'users');
+    const usersQuery = query(usersCollection, where('name', '>=', this.searchText), where('name', '<=', this.searchText + '\uf8ff'));
+    return collectionData(usersQuery, { idField: 'id' }).pipe(
+      map(users => {
+        this.userSearchResults = [];
+        users.forEach(user => {
+          this.userSearchResults.push(user.id);
+        });
+        return users;
+      })
+    );
+  }
+
+
+  pushSelectedUserToArray(userId: string) {
+    if (!this.selectedUser.includes(userId)) {
+      this.selectedUser.push(userId);
+      console.log('User pushed into Array', this.selectedUser);
+    }
+    this.memberSearchActive = false;
+  }
+
+  removeSelectedUserFromArray(userId: string) {
+    const index = this.selectedUser.indexOf(userId);
+    if (index > -1) {
+      this.selectedUser.splice(index, 1);
+    }
+    console.log('User removed from Array', this.selectedUser);
+  }
+  
 }
+
