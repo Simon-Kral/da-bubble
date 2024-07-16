3e9884dfd93408fa5fb09f16b1de9015cebdf85f
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-16 15:14:36 +0200

    feat: created shared component "channel-member-selection"
    A new shared component called "channel-member-selection" has been created. This is mainly used as a template to render the search queries entered via the input field when creating a channel or subsequently adding members.

---
+++ src/app/post-login/shared/channel-member-selection/channel-member-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-member-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './channel-member-selection.component.html',
+  styleUrl: './channel-member-selection.component.scss'
+})
+export class ChannelMemberSelectionComponent {
+
+}
