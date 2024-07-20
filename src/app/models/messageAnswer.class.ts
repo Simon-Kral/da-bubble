54374956379a642aaf8122b396ba85e6509f7399
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-20 23:29:18 +0200

    refactor: The messageAnswer model has been adapted to the requirements

--- src/app/models/messageAnswer.class.ts
+++ src/app/models/messageAnswer.class.ts
@@ -5,18 +5,18 @@ export class MessageAnswer {
   messageAnswerId: string;
   text: string;
   messageId: string;
-  user: User;
   date: string;
   time: string;
+  messageSendBy: string;
   reactions: Reaction[];
 
   constructor(obj?: any) {
     this.messageAnswerId = obj ? obj.id : '';
     this.text = obj ? obj.text : '';
     this.messageId = obj ? obj.messageId : '';
-    this.user = obj ? obj.user : '';
     this.date = obj ? obj.date : '';
     this.time = obj ? obj.time : '';
+    this.messageSendBy = obj ? obj.messageSendBy : '';
     this.reactions = obj ? obj.reactions : [];
   }
 }
