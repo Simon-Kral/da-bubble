9b8d8bc7bf751032460de1a73308185c3ebea5d3
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-20 21:47:05 +0200

    refactor: added jsdoc & deleted console.log statements

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -15,12 +15,14 @@ export class SearchService {
 	firebaseService = inject(FirebaseService);
 	chatService = inject(ChatService);
 	router: any;
-	
+	//subscriptions 
 	userSearchResults: string[] = [];
 	channelSearchResults: string[] = [];
 	messageSearchResults = [];
 	private channelSubscription: Subscription = new Subscription();
 	private userSubscription: Subscription = new Subscription();
+
+	//selected items
 	selectedUser: string[] = [];
 	selectedChannel: string = '';
 	selectedMessage = [];
@@ -31,26 +33,20 @@ export class SearchService {
 	
 	constructor() {}
 
-
-
-
-	// code for user search
-
+// code for user search
   /**
    * Searches the users list for documents where the name field matches or contains the search text.
    * @returns An Observable of the search results.
-   *  to-do why channelId ?????????
    */
-  searchUsersByName(channelId: string): Observable<User[]> {
+  searchUsersByName(chanId: string): Observable<User[]> {
     const filteredUsers = this.firebaseService.userList.filter(
       (user) =>
-        user.name.toLowerCase().includes(this.searchText.toLowerCase()) &&!user.channels.includes(channelId) 
+        user.name.toLowerCase().includes(this.searchText.toLowerCase()) &&!user.channels.includes(chanId) 
 	  	&&user.userId !== this.firebaseService.currentUserId &&!this.selectedUser.includes(user.userId)	
     );
     this.userSearchResults = filteredUsers.map((user) => user.userId);
     return of(filteredUsers);
   }
-
 
 	/**
 	 * Handles the user focus event and performs a search for users by name.
@@ -60,14 +56,7 @@ export class SearchService {
 	onUserFocus(searchText: string, channelId: string = '') {
 		this.memberSearchActive = true;
 		this.searchText = searchText || '';
-		console.log('Search text received by searchService:', this.searchText);
-
 		this.searchUsersByName(channelId).subscribe((users) => {
-			console.log('Search results:', users);
-			console.log(
-				'userSearchResults array contains:',
-				this.userSearchResults
-			);
 		});
 	}
 
@@ -79,29 +68,42 @@ export class SearchService {
 	 */
 	onUserSearch(searchText: string, channelId: string = '') {
 		this.searchText = searchText || '';
-		console.log('Search text received by searchService:', this.searchText);
-
 		this.userSubscription = this.searchUsersByName(channelId).subscribe();
 		this.memberSearchActive = this.searchText.trim().length > 0;
 	}
 
+  /**
+   * Adds a user ID to the `selectedUser` array if it is not already included.
+   * If the user ID is added, it logs the updated `selectedUser` array.
+   * Also sets `memberSearchActive` to `false` after attempting to add the user ID.
+   * @param {string} userId - The ID of the user to add to the `selectedUser` array.
+   */
 	pushSelectedUserToArray(userId: string) {
 		if (!this.selectedUser.includes(userId)) {
 			this.selectedUser.push(userId);
-			console.log('User pushed into Array', this.selectedUser);
 		}
 		this.memberSearchActive = false;
 	}
 
+  /**
+   * Removes a user ID from the `selectedUser` array if it exists.
+   * Logs the updated `selectedUser` array after the user ID is removed.
+   * @param {string} userId - The ID of the user to remove from the `selectedUser` array.
+   */
 	removeSelectedUserFromArray(userId: string) {
 		const index = this.selectedUser.indexOf(userId);
 		if (index > -1) {
 			this.selectedUser.splice(index, 1);
 		}
-		console.log('User removed from Array', this.selectedUser);
 	}
 
-
+	/**
+   * Searches for users by their email address.
+   * Filters the `userList` in `firebaseService` to include users whose email includes the `searchText` and excludes the current user.
+   * Updates the `userSearchResults` array with the user IDs of the filtered users.
+   * Returns an Observable of the filtered user array.
+   * @returns {Observable<User[]>} An Observable emitting the array of filtered users.
+   */
 	searchUsersByEmail(): Observable<User[]> {
 		const filteredUsers = this.firebaseService.userList.filter((user) => 
 		  user.email.toLowerCase().includes(this.searchText.toLowerCase()) && 
@@ -111,17 +113,19 @@ export class SearchService {
 		return of(filteredUsers);
 	  }
 
+  /**
+   * Handles the email search input.
+   * Sets the `searchText` and subscribes to the user search results.
+   * Updates the `memberSearchActive` flag based on whether the search text is non-empty.
+   * @param {string} searchText - The text input used for searching users by email.
+   */
 	onEmailSearch(searchText: string) {
 		this.searchText = searchText || '';
-		console.log('Search text received by searchService:', this.searchText);
 		this.userSubscription = this.searchUsersByEmail().subscribe();
-		console.log('User search results:', this.userSearchResults);
 		this.memberSearchActive = this.searchText.trim().length > 0;
 	}
 
 	// code for channel search 
-
-
 	/**
 	 * Searches the channel list for documents where the name field matches or contains the search text.
 	 * @returns An Observable of the search results.
@@ -130,29 +134,30 @@ export class SearchService {
 	const filteredChannels = this.firebaseService.channelList.filter(
 		(channel) =>
 			channel.name.toLowerCase().includes(this.searchText.toLowerCase()) 
-			&& channel.chanId !== this.selectedChannel
-			
+			&& channel.chanId !== this.selectedChannel	
 		);
 		this.channelSearchResults = filteredChannels.map((channel) => channel.chanId);
 		return of(filteredChannels);
 	}
 	  
-
-
-
+  /**
+   * Handles the channel search input.
+   * Sets the `searchText`, activates the channel search, and subscribes to the channel search results.
+   * @param {string} searchText - The text input used for searching channels by name.
+   */
 	onChannelSearch(searchText: string) {
 		this.channelSearchActive = true;
 		this.searchText = searchText.trim();
-		console.log('Search text received by searchService:', this.searchText);
 		this.channelSubscription = this.searchChannelsByName().subscribe();
 	}
 
-
+ /**
+   * Handles the focus event for the channel search input.
+   * Activates the channel search and subscribes to the channel search results.
+   */
 	onChannelFocus() {
 		this.channelSearchActive = true;
 		this.searchChannelsByName().subscribe((channels) => {
-		console.log('All channels:', channels);
-		console.log('channelSearchResults array contains:', this.channelSearchResults);
 		});
 	}
 
