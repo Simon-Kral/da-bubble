260c4ba24ce681b7e95451c82153445bd136dd40
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-23 22:43:51 +0200

    refactor: recent changes for adding reactions

--- src/app/models/reaction.class.ts
+++ src/app/models/reaction.class.ts
@@ -1,13 +1,13 @@
 export class Reaction {
   reactionId: string;
-  user: string;
+  user: string[];
   message_id: string;
   amount: number;
   nativeEmoji: string;
 
   constructor(obj?: any) {
     this.reactionId = obj ? obj.id : '';
-    this.user = obj ? obj.user : '';
+    this.user = obj ? obj.users : [];
     this.message_id = obj ? obj.post_id : '';
     this.amount = obj ? obj.amount : 0;
     this.nativeEmoji = obj ? obj.emoji : '';
