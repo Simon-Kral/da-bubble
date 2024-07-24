ef644a56478a34e518ff215c2e093c02b54cdc92
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-24 13:21:31 +0200

    refactor: reactionService adding/deleting/updating messages with reactin object
    The following has been implemented in the reaction service:
    2 fixed reaction objects "ThumpUp +ThumpDown"
    
    These fixed reactions are displayed as native emojis when hovering in the menu.
    
    When a user clicks on it, the variable currentMessageId is saved with the msg.messageId. The "addSpecificReaction" method is then executed. It is given the messageId and one of the fixed objects.
    
    The message doc is updated accordingly.
    
    The updateReactionAmount method is then called with the "increase" operation, where the reactionID is used to check whether the specific reaction exists. If it exists, the amount is increased by 1. If the specific reaction does not exist, it is saved as an object in the message document in the reactions array.
    
    The "handleReaction" method is called via the emojiPicker. It is given the messageId and the emojiEvent. The reaction object is created according to the event. The "addSpecificReaction" method is then called and takes over the logic described above.
    
    The "deleteReaction" method is executed when you click on a reaction rendered under the chat message.

--- src/app/services/reactions/reaction.service.ts
+++ src/app/services/reactions/reaction.service.ts
@@ -18,6 +18,90 @@ export class ReactionService {
 
   constructor() { }
 
+	currentMessageId: string = '';
+  
+  setReactionThumbUp: Reaction = {
+	reactionId: '1F44D',
+	user: [this.firebaseService.currentUserId],
+	message_id: this.currentMessageId,
+	amount: 1,
+	nativeEmoji: '👍',
+  };
+  setReactionThumbDown: Reaction = {
+	reactionId: '1F44E',
+	user: [this.firebaseService.currentUserId],
+	message_id: this.currentMessageId,
+	amount: 1,
+	nativeEmoji: '👎',
+  };
+
+
+/**
+ * Adds a new reaction to a message.
+ *
+ * @param {Reaction} newReaction - The new reaction object to be added.
+ * @param {string} messageId - The ID of the message to which the reaction is to be added.
+ * @returns {Promise<void>} A promise that resolves when the reaction is successfully added.
+ * @throws Will throw an error if there is an issue updating the Firestore document.
+ */
+async addSpecificReaction(messageId: string, reaction:Reaction): Promise<void> {
+	console.log('reaction:', reaction);
+	try {
+	  const messageDocRef = doc(
+		this.firestore, `${this.chatService.mainCollection}/${this.chatService.docRef}/messages/${messageId}`
+	  );
+	  await updateDoc(messageDocRef, {
+		reactions: arrayUnion(reaction),
+	  });
+	  console.log('reaction added', reaction );
+
+	  await this.updateReactionAmount(reaction.reactionId, messageId, 'increase');
+	} catch (error) {
+	  console.error('Error updating message text:', error);
+	  throw error;
+	}
+  }
+
+
+
+
+/**
+ * Deletes a currentUser reaction from a message document which got added before by the current user.
+ * 
+ * @param {string} reactionId - The ID of the reaction to be deleted.
+ * @param {string} messageId - The ID of the message document containing the reaction.
+ * @returns {Promise<void>} A promise that resolves when the reaction is deleted or updated.
+ * @throws Will throw an error if there is an issue accessing or updating the Firestore document.
+ */
+async deleteReaction(reactionId: string, messageId: string): Promise<void> {
+	try {
+	  const messageDocRef = doc(this.firestore, `${this.chatService.mainCollection}/${this.chatService.docRef}/messages/${messageId}`);
+	  const messageDoc = await getDoc(messageDocRef);
+  
+	  if (messageDoc.exists()) {
+		const data = messageDoc.data();
+		if (data && data['reactions']) {
+		  const reaction = data['reactions'].find((r: Reaction) => r.reactionId === reactionId && r.user.includes(this.firebaseService.currentUserId));
+		  if (reaction) {
+			if (reaction.amount > 1) {
+			  // If the amount is greater than 1, decrease it by 1
+			  await this.updateReactionAmount(reactionId, messageId, 'decrease');
+			} else {
+			  // If the amount is 1, remove the reaction
+			  const updatedReactions = data['reactions'].filter((r: Reaction) => r.reactionId !== reactionId);
+			  await updateDoc(messageDocRef, { reactions: updatedReactions });
+			  console.log('Reaction deleted');
+			}
+		  } else {
+			console.log('No reaction found for the current user');
+		  }
+		}
+	  }
+	} catch (error) {
+	  console.error('Error deleting reaction:', error);
+	  throw error;
+	}
+  }
 
 
 /**
@@ -27,12 +111,12 @@ export class ReactionService {
  * @param {string} messageId - The ID of the message to which the reaction is to be added or updated.
  */
 handleReaction(event: any, messageId: string) {
-	console.log('event:', event);
+	console.log('event:', event);          
 	console.log('messageId:', messageId);
-  
 	let nativeEmoji = event.emoji.native;
 	console.log('nativeEmoji:', nativeEmoji);
-	let reactionId = event.emoji.id;
+	let reactionId = event.emoji.unified;
+
 	let newReaction: Reaction = {
 	  reactionId: reactionId,
 	  user: [this.firebaseService.currentUserId],
@@ -43,67 +127,9 @@ handleReaction(event: any, messageId: string) {
   
 	console.log('newReaction:', newReaction);
   
-	// Check if the reaction already exists and if the current user already reacted
-	this.checkIfReactionExistsForUser(reactionId, messageId).then(reactionExists => {
-	  if (reactionExists) {
-		// If it exists, increase the amount
-		this.updateReactionAmount(reactionId, messageId, 'increase');
-	  } else {
-		// If it does not exist, add the new reaction
-		this.addReaction(newReaction, messageId);
-	  }
-	});
+	this.addSpecificReaction(messageId, newReaction)
+
 	this.showEmojiPicker = false;
-  }
-
-/**
- * Adds a new reaction to a message.
- *
- * @param {Reaction} newReaction - The new reaction object to be added.
- * @param {string} messageId - The ID of the message to which the reaction is to be added.
- * @returns {Promise<void>} A promise that resolves when the reaction is successfully added.
- * @throws Will throw an error if there is an issue updating the Firestore document.
- */
-async addReaction(newReaction: Reaction, messageId: string): Promise<void> {
-	console.log('reaction:', newReaction);
-	try {
-	  const messageDocRef = doc(
-		this.firestore, `${this.chatService.mainCollection}/${this.chatService.docRef}/messages/${messageId}`
-	  );
-	  await updateDoc(messageDocRef, {
-		reactions: arrayUnion(newReaction),
-	  });
-	  console.log('reaction added', newReaction);
-	} catch (error) {
-	  console.error('Error updating message text:', error);
-	  throw error;
-	}
-  }
-
-/**
- * Checks if the current user has already added a reaction with the specified ID to the given message.
- * 
- * @param {string} reactionId - The ID of the reaction to check for.
- * @param {string} messageId - The ID of the message to check the reaction in.
- * @returns {Promise<boolean>} A promise that resolves to true if the user has already added the reaction, false otherwise.
- * @throws Will throw an error if there is an issue accessing the Firestore document.
- */
-async checkIfReactionExistsForUser(reactionId: string, messageId: string): Promise<boolean> {
-	try {
-	  const messageDocRef = doc(this.firestore, `${this.chatService.mainCollection}/${this.chatService.docRef}/messages/${messageId}`);
-	  const messageDoc = await getDoc(messageDocRef);
-  
-	  if (messageDoc.exists()) {
-		const data = messageDoc.data();
-		if (data && data['reactions']) {
-		  return data['reactions'].some((reaction: Reaction) => reaction.reactionId === reactionId && reaction.user.includes(this.firebaseService.currentUserId));
-		}
-	  }
-	  return false;
-	} catch (error) {
-	  console.error('Error checking reaction existence for user:', error);
-	  throw error;
-	}
   }
 
 
@@ -147,41 +173,5 @@ async updateReactionAmount(reactionId: string, messageId: string, operation: 'in
 	}
   }
 
-/**
- * Deletes a currentUser reaction from a message document which got added before by the current user.
- * 
- * @param {string} reactionId - The ID of the reaction to be deleted.
- * @param {string} messageId - The ID of the message document containing the reaction.
- * @returns {Promise<void>} A promise that resolves when the reaction is deleted or updated.
- * @throws Will throw an error if there is an issue accessing or updating the Firestore document.
- */
-async deleteReaction(reactionId: string, messageId: string): Promise<void> {
-  try {
-    const messageDocRef = doc(this.firestore, `${this.chatService.mainCollection}/${this.chatService.docRef}/messages/${messageId}`);
-    const messageDoc = await getDoc(messageDocRef);
 
-    if (messageDoc.exists()) {
-      const data = messageDoc.data();
-      if (data && data['reactions']) {
-        const reaction = data['reactions'].find((r: Reaction) => r.reactionId === reactionId && r.user.includes(this.firebaseService.currentUserId));
-        if (reaction) {
-          if (reaction.amount > 1) {
-            // If the amount is greater than 1, decrease it by 1
-            await this.updateReactionAmount(reactionId, messageId, 'decrease');
-          } else {
-            // If the amount is 1, remove the reaction
-            const updatedReactions = data['reactions'].filter((r: Reaction) => r.reactionId !== reactionId);
-            await updateDoc(messageDocRef, { reactions: updatedReactions });
-            console.log('Reaction deleted');
-          }
-        } else {
-          console.log('No reaction found for the current user');
-        }
-      }
-    }
-  } catch (error) {
-    console.error('Error deleting reaction:', error);
-    throw error;
-  }
-}
 }
