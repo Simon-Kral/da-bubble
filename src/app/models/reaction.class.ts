96f9b4e79dcf9f0980b6f04c598f2a7bcd22033f
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-27 22:38:17 +0200

    refactor: adjusted reactions class

--- src/app/models/reaction.class.ts
+++ src/app/models/reaction.class.ts
@@ -1,14 +1,14 @@
 export class Reaction {
   reactionId: string;
   user: string[];
-  message_id: string;
+  messageId: string;
   amount: number;
   nativeEmoji: string;
 
   constructor(obj?: any) {
     this.reactionId = obj ? obj.id : '';
     this.user = obj ? obj.users : [];
-    this.message_id = obj ? obj.post_id : '';
+    this.messageId = obj ? obj.messageId : '';
     this.amount = obj ? obj.amount : 0;
     this.nativeEmoji = obj ? obj.emoji : '';
   }
