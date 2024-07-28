3f2ce80e66572fa5c4282239080cdcf56c4d2473
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-07-28 18:31:07 +0200

    implemented logout() into mobile.user-menu

--- src/app/post-login/home/mobile-user-menu/mobile-user-menu.component.ts
+++ src/app/post-login/home/mobile-user-menu/mobile-user-menu.component.ts
@@ -1,6 +1,11 @@
 import { Component, inject, Inject } from '@angular/core';
 import { CommunicationService } from '../../../services/communication/communication.service';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/authentication/auth.service';
+import { AppComponent } from '../../../app.component';
+import { FirebaseService } from '../../../services/firebase/firebase.service';
+import { ChatService } from '../../../services/chat/chat.service';
+import { ThreadService } from '../../../services/thread/thread.service';
 
 @Component({
   selector: 'app-mobile-user-menu',
@@ -12,10 +17,56 @@ import { CommonModule } from '@angular/common';
 export class MobileUserMenuComponent {
 
   communicationService = inject(CommunicationService);
+  authService = inject(AuthService);
+  appComponent = inject(AppComponent);
+  firebaseService = inject(FirebaseService);
+  chatService = inject(ChatService);
+  threadService = inject(ThreadService);
 
+  
   activeMenu: string = '';
 
   setActiveMenu(menu: string) {
     this.activeMenu = menu;
   }
+
+  async logout(): Promise<void> {
+		// to-do OPTIONAL update user status to offline when close tab
+		try {	
+		if (this.authService.firebaseAuth.currentUser?.email != undefined) {
+			this.appComponent.notificateUser('Abmelden');
+
+		await this.firebaseService.unsubscribeAllLists();
+		await this.chatService.unsubscribeAllLists();
+		await this.threadService.unsubscribeAllLists();
+		await this.firebaseService.updateUserStatus(false);
+
+			this.authService.logout().subscribe({
+				next: () => {
+					sessionStorage.clear();
+				},
+				error: (err) => {
+					console.log(err);
+				},
+			});
+		} else {
+		
+			await this.firebaseService.unsubscribeAllLists();
+			await this.chatService.unsubscribeAllLists();
+			await this.threadService.unsubscribeAllLists();
+			await this.firebaseService.updateUserStatus(false);	
+
+			this.authService.logout().subscribe({
+				next: () => {
+					sessionStorage.clear();
+				},
+				error: (err) => {
+					console.log(err);
+				},
+			});
+		}
+	} catch (error) {
+		console.error('Error during cleanup before logout:', error);
+	  }
+	}
 }
